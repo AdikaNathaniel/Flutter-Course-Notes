327172f4b5c1d0fc0b5e202b727d7b75aef0c089
Nathaniel <najadika@st.ug.edu.gh>
2026-01-17 20:54:19 +0000

    Initial commit

--- Lessons.dart
+++ Lessons.dart
@@ -3,3 +3,7 @@
 3.Compilation is the transformation of source code which is human readable into machine code which is computer executable. 
 4.Native code is code specific to a particular platform like Swift is native for Ios. 
 5.So flutter renders the app and not the platform. 
+6.Recall that Andriod Studio or Vscode can be used as IDE for a flutter
+project.
+7.The emulator needs Andriod Studio to works with it.
+
