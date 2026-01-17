faa16e3885b6b213eadac8e3c7f142e16752dbb5
Nathaniel <najadika@st.ug.edu.gh>
2026-01-17 20:49:10 +0000

    Initial commit

---
+++ Lessons.dart
@@ -0,0 +1,5 @@
+1.Flutter is an SDK for building applications for various platforms from one codebase. 
+2.Dart is the programming language. 
+3.Compilation is the transformation of source code which is human readable into machine code which is computer executable. 
+4.Native code is code specific to a particular platform like Swift is native for Ios. 
+5.So flutter renders the app and not the platform. 
