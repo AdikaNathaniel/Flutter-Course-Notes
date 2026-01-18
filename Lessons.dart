e4f13725d3c9b62f311433f0cc868e1dddc83d11
Nathaniel <najadika@st.ug.edu.gh>
2026-01-18 17:47:47 +0000

    notes

--- Lessons.dart
+++ Lessons.dart
@@ -6,4 +6,19 @@
 6.Recall that Andriod Studio or Vscode can be used as IDE for a flutter
 project.
 7.The emulator needs Andriod Studio to works with it.
-
+8.To enable flutter for web :
+  -- flutter config --enable-web
+9.The materialApp is the root of the widget Tree.
+10.Same way you wrap a widget with another widget,that
+is the same way to remove a widget.
+Press Alt and Enter at once for this.
+11.Scaffold is the skeleton on which other widgets are built.
+12.Release Mode is for production whilst Debug mode is 
+for Testing purposes.
+   debugShowCheckedModeBanner : false
+13.Flutter Icon is used to change the icon that shows to open the app.
+14.Quick boot is when after Andriod Studio is boot,
+it retrieves all old files open but for Cold boot all no-saved files
+are lost.
+15.Hot reload updates a change instantly but Hot Restart starts the app again
+with the new updates in the app.
