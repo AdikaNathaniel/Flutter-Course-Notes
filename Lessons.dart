21aba249e9647c99d4e6f3fc4981d220fb999b16
Nathaniel <najadika@st.ug.edu.gh>
2026-01-18 20:42:00 +0000

    notes

--- Lessons.dart
+++ Lessons.dart
@@ -291,3 +291,11 @@ void main(){
     }
 
 // Output Console will be true.
+35.Print all alternate numbers from 1 to 1000
+    void main(){
+        for(int i=1;i<=1000;i=i+2){
+            print(i);
+        }
+    }
+                   STATE MANAGEMENT
+36.
